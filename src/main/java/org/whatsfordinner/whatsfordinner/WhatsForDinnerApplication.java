4cdc96183511083584e5759b2e39ae19f7947e51
Bianca Sava <biancasava07@gmail.com>
2026-02-23 16:50:21 +0200

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/org/whatsfordinner/whatsfordinner/WhatsForDinnerApplication.java
@@ -0,0 +1,13 @@
+package org.whatsfordinner.whatsfordinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsForDinnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsForDinnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/whatsfordinner/whatsfordinner/WhatsForDinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whatsfordinner.whatsfordinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsForDinnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
